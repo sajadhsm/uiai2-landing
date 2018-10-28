09e0d167a48bf9281cff19c63ac4d6685cec2900
Sajad Hashemian <sajadhsm@gmail.com>
2018-10-28 18:30:49 +0330

    Add basic Description section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Hero from './components/Hero/Hero';
+import Description from './components/Description/Description';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Hero />
+        <Description />
       </div>
     );
   }

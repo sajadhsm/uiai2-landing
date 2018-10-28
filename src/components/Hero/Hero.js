e456f42c37bb679c6dc0158d345f6285c8721be7
Sajad Hashemian <sajadhsm@gmail.com>
2018-10-28 15:55:54 +0330

    Add basic logo to hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,12 @@
 import React, { PureComponent } from 'react';
 import styles from './Hero.module.css';
+import logo from './Logo.png';
 
 class Hero extends PureComponent {
   render() {
     return (
       <section className={styles.hero}>
+        <img src={logo} alt="UIAI2 Logo"/>
         <h1>دومین دوره مسابقات چالشی<br/>هوش مصنوعی دانشگاه اصفهان</h1>
       </section>
     );

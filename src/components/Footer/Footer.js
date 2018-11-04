bbd66dc7ef31ac1e006646b9d65b8c0e5a9c7e4b
Sajad Hashemian <sajadhsm@gmail.com>
2018-11-04 21:05:31 +0330

    Add social links to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,30 @@
 import React, { PureComponent } from 'react';
 import styles from './Footer.module.css';
 
+import twitter from './twitter-logo.png';
+import instagram from './instagram-logo.png';
+import telegram from './telegram-logo.png';
+
 class Footer extends PureComponent {
   render() {
     return (
       <footer className={styles.footer}>
         <p>دومین دوره مسابقات چالشی هوش مصنوعی دانشگاه اصفهان</p>
-        <p>#UIAI2018</p>
+
+        <div className={styles.socialLinks}>
+          <a href="https://t.me/ui_acm" title="کانال انجمن ACM">
+            <img src={telegram} alt="Telegram icon"/>
+          </a>
+          <a href="https://Instagram.com/uiai_2018" title="صفحه اینستاگرام uiai2018">
+            <img src={instagram} alt="Instagram icon"/>
+          </a>
+          <a href="https://twitter.com/uiai2018" title="صفحه توییتر uiai2018">
+            <img src={twitter} alt="Twitter icon"/>
+          </a>
+          <a href="https://t.me/uicvgame" title="کانال مرکز بازی سازی">
+            <img src={telegram} alt="Telegram icon"/>
+          </a>
+        </div>
       </footer>
     );
   }

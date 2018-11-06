8d064e2817e86875275b127c03e1749ca14c5b6b
Sajad Hashemian <sajadhsm@gmail.com>
2018-11-06 10:30:43 +0330

    Change footer social links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,17 +12,14 @@ class Footer extends PureComponent {
         <p>دومین دوره مسابقات چالشی هوش مصنوعی دانشگاه اصفهان</p>
 
         <div className={styles.socialLinks}>
-          <a href="https://t.me/ui_acm" title="کانال انجمن ACM">
+          <a href="https://t.me/uiai2018" title="کانال تلگرام uiai2018">
             <img src={telegram} alt="Telegram icon"/>
           </a>
           <a href="https://Instagram.com/uiai_2018" title="صفحه اینستاگرام uiai2018">
             <img src={instagram} alt="Instagram icon"/>
           </a>
-          <a href="https://twitter.com/uiai2018" title="صفحه توییتر uiai2018">
+          <a href="https://twitter.com/ui_acm" title="صفحه توییتر uiai2018">
             <img src={twitter} alt="Twitter icon"/>
-          </a>
-          <a href="https://t.me/uicvgame" title="کانال مرکز بازی سازی">
-            <img src={telegram} alt="Telegram icon"/>
           </a>
         </div>
       </footer>

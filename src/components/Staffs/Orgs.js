622543abd369ac071086a9309cfb8c3393653233
Sajad Hashemian <sajadhsm@gmail.com>
2018-11-12 00:14:58 +0330

    Add organizations logos

---
+++ src/components/Staffs/Orgs.js
@@ -0,0 +1,18 @@
+import acm from './images/acm.png';
+import ui from './images/ui_logo.png';
+import bazisazi from './images/bazisazi.jpg';
+
+export default [
+  {
+    name: 'انجمن علمی ACM',
+    avatar: acm
+  },
+  {
+    name: 'مرکز تخصصی بازی‌های رایانه‌ای',
+    avatar: bazisazi
+  },
+  {
+    name: 'دانشکده مهندسی کامپیوتر',
+    avatar: ui
+  },
+];
--- src/components/Staffs/Staffs.js
+++ src/components/Staffs/Staffs.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import styles from './Staffs.module.css';
 import Persons from './Persons';
+import Orgs from './Orgs';
 
 class Staffs extends PureComponent {
   render() {
@@ -8,6 +9,17 @@ class Staffs extends PureComponent {
       <section className={styles.staffsSection}>
         <div className="wrapper">
           <h2 className="section-title">برگزار‌کنندگان</h2>
+          <div className={styles.staffs}>
+            {Orgs.map((org, index) => (
+              <div className={styles.staff} key={index}>
+                <img
+                  className={styles.staffImage}
+                  src={org.avatar}
+                  alt={`تصویر ${org.name}`} />
+                <h5 className={styles.staffName}>{org.name}</h5>
+              </div>
+            ))}
+          </div>
           <div className={styles.staffs}>
             {Persons.map((person, index) => (
               <div className={styles.staff} key={index}>
